f0172cda2010864a3caa38f65ad9fb1483ffe94d
LeoYaMM <y.leobardomartinez@gmail.com>
2023-12-19 12:34:34 -0600

    Update indexCola.html and scriptCola.js

--- scriptCola.js
+++ scriptCola.js
@@ -1,5 +1,4 @@
-//Script para mostrar las imagenes cuando se pulse un botón y mostrar las imagenes
-// Obtener los elementos
+// Intercambio de imágenes
 var pushButton = document.getElementById("pushButton");
 var popButton = document.getElementById("popButton");
 var imagenPop = document.getElementById("imagenPop");
@@ -7,14 +6,55 @@ var imagenPush = document.getElementById("imagenPush");
 
 popButton.addEventListener("click", function(event) {
     imagenPush.style.display= "none"
-    event.preventDefault(); // Prevenir el comportamiento predeterminado
+    event.preventDefault();
     imagenPop.style.display = "block"; 
 });
 
 pushButton.addEventListener("click", function(event) {
     imagenPop.style.display= "none"
-    event.preventDefault(); // Prevenir el comportamiento predeterminado
+    event.preventDefault();
     imagenPush.style.display = "block";
 });
 
+//funcionalidad de la cola
+function crearNuevaCola() {
+    const cola = document.getElementById('cola');
+    while (cola.firstChild) {
+        cola.removeChild(cola.firstChild);
+    }
+}
 
+function agregarALaCola(elemento) {
+    const cola = document.getElementById('cola');
+    const nuevoElemento = document.createElement('div');
+    nuevoElemento.classList.add('elemento-cola');
+    nuevoElemento.textContent = elemento;
+    cola.appendChild(nuevoElemento);
+}
+
+function guardarCola() {
+    const elementos = [];
+    document.querySelectorAll('.elemento-cola').forEach(elemento => {
+        elementos.push(elemento.textContent);
+    });
+    localStorage.setItem('cola', JSON.stringify(elementos));
+}
+
+function cargarColaGuardada() {
+    const colaGuardada = JSON.parse(localStorage.getItem('cola'));
+    if (colaGuardada) {
+        crearNuevaCola(); // Limpia la cola actual antes de cargar la guardada
+        colaGuardada.forEach(elemento => {
+            agregarALaCola(elemento);
+        });
+    }
+}
+
+pushButton.addEventListener('click', function() {
+    const elemento = document.getElementById('elementoCola').value;
+    if (elemento) {
+        agregarALaCola(elemento);
+        guardarCola();
+    }
+    mostrarImagen('push');
+});
